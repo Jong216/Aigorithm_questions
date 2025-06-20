005b019ef1e7dad592c6f3bdd3cdccdef7630bf0
Jinlong Zhou <160200111+Jong216@users.noreply.github.com>
2025-06-20 22:22:05 +0800

    Update lt128.py

--- Leetcoded/lt128.py
+++ Leetcoded/lt128.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-      '''
-      时间复杂度：O(n)
-      空间复杂度：O(1)
-      '''
         if not nums:
             return 0
-        nums.sort()
-        res = 1
-        maxlen = 1
-        for i in range(1, len(nums)):
-            if nums[i] - 1 == nums[i-1]:
-                res += 1
-            elif nums[i] - 1 > nums[i-1]:
-                res = 1
-                
-            maxlen = max(maxlen, res)
-        
-        return maxlen
+        # nums.sort() 因为快速排序的时间复杂度是O（nlogn）
+        # 所以不能排序
+        # 可以采用哈希表记录下来
+        st = set(nums) # 导致有空间复杂度
+        ans = 0
+        for x in st: # 保证O(n)
+            if x-1 in st: 
+                # 有重复的
+                continue
+            y = x + 1
+            while y in st:
+                y += 1
+            ans = max(ans, y-x)
+        return ans
             
